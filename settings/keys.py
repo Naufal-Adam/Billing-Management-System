aa53dbc306b6cf21e7b316f7e1ec435b18103e4a
Naufal-Adam <madamfirdaus4@gmail.com>
2023-08-17 20:46:44 +0700

    Revisi Ke 4

--- settings/keys.py
+++ settings/keys.py
@@ -1,4 +1,4 @@
-account_sid = 'ACe0b34007a39c892e8011a3f515d522fa'
-auth_token = '36757655bf97237324837eb400e00698'
-twilio_number = '+17069838202'
-target_number = '+6285784739850'
+account_sid = ''
+auth_token = ''
+twilio_number = ''
+target_number = ''
